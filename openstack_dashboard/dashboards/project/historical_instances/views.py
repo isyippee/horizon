e4e2c122b400f8de21eaa79ef2f9a8734c63ea5f
LiuYang <yippeetry@gmail.com>
2016-05-23 15:15:02 +0800

    fix a bug, about mode import
    Signed-off-by: LiuYang <yippeetry@gmail.com>

--- openstack_dashboard/dashboards/project/historical_instances/views.py
+++ openstack_dashboard/dashboards/project/historical_instances/views.py
@@ -5,18 +5,21 @@ from horizon import tables
 from openstack_dashboard import api
 
 from openstack_dashboard.dashboards.project.historical_instances import tables as project_tables
+from horizon import exceptions
+from django.utils.translation import ugettext_lazy as _
+
 
 class IndexView(tables.DataTableView):
     # A very simple class-based view...
     table_class = project_tables.InstancesTable
     template_name = 'project/historical_instances/index.html'
 
-    #def get_data(self, request, context, *args, **kwargs):
-        # Add data to the context here...
+    # def get_data(self, request, context, *args, **kwargs):
+    # Add data to the context here...
     #    return context
 
     def get_data(self):
-        search_opts={'deleted':True}
+        search_opts = {'deleted': True}
         try:
             instances, self._more = api.nova.server_list(
                 self.request,
@@ -27,5 +30,5 @@ class IndexView(tables.DataTableView):
             exceptions.handle(self.request,
                               _('Unable to retrieve instances.'))
         print instances
-        #instances = []
+        # instances = []
         return instances
